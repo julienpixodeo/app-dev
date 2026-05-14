e5663c878faa3a4e024d3d9d87980cd97afe9f97
Dev <julien@pixodeo.net>
2026-05-14 11:12:17 +0700

    deploy1

--- app/routes/app.test-page.tsx
+++ app/routes/app.test-page.tsx
@@ -51,7 +51,7 @@ export default function TestPage() {
               loading={loading}
               onClick={deleteCustomer}
             >
-              Delete Customer
+              Delete Customer ! test
             </Button>
           </div>
         </div>
